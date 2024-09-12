1bb0da7b173823b864e6654ac7c7bc079b82d009
Avner Duchovni <avnerd@checkpoint.com>
2024-09-12 14:04:24 +0300

    Add typing and docstrings to gpt_query.py

--- app/gpt_query.py
+++ app/gpt_query.py
@@ -2,33 +2,38 @@
 Module for demonstrating basic usage of GPT via API
 """
 
+import logging
+
+from openai.types.chat.chat_completion import ChatCompletion  # Just for typing
+
 from openai_tools import openai_client
 from env import AZURE_OPENAI_API_MODEL
 
-import logging
 
-
-def query_gpt(query):
+def query_gpt(query: str, model=AZURE_OPENAI_API_MODEL) -> ChatCompletion:
+    """Send a query to OpenAI and return the response"""
     logging.info(f"Querying GPT for: {query}")
     res = openai_client.chat.completions.create(
-        model=AZURE_OPENAI_API_MODEL, messages=[{"role": "user", "content": query}]
+        model=model, messages=[{"role": "user", "content": query}]
     )
-
     return res
 
 
-def parse_gpt_response(res):
+def parse_gpt_response(res: ChatCompletion) -> str:
+    """Parse a ChatCompletion response from OpenAI"""
     parsed_res = res.choices[0].message.content
     logging.info(f"Response parsed: {parsed_res}")
     return parsed_res
 
 
-def query_and_parse(query):
+def query_and_parse(query: str) -> str:
+    """Query GPT and parse the response"""
     res = query_gpt(query)
     return parse_gpt_response(res)
 
 
-def demo():
+def demo() -> None:
+    """Demonstrate invocation of `query_and_parse`"""
     query = "What is 10 times 10"
     parsed_res = query_and_parse(query)
     print(f"Parsed response: {parsed_res}")
@@ -36,4 +41,3 @@ def demo():
 
 if __name__ == "__main__":
     demo()
-
